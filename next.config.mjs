291a2178a86b92971dcf92271159c680362110e6
slimsherpa <rileyjadamson@gmail.com>
2025-03-18 20:49:27 -0600

    cleaned up build

--- next.config.mjs
+++ next.config.mjs
@@ -12,86 +12,22 @@ const nextConfig = {
       },
     ],
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: 'standalone',
   experimental: {
-    optimizePackageImports: ['@mui/icons-material', '@mui/material'],
     typedRoutes: true,
   },
-  poweredByHeader: false,
-  generateEtags: true,
-  compress: true,
-  productionBrowserSourceMaps: false,
-  distDir: '.next',
-  cleanDistDir: true,
-  webpack: (config, { dev, isServer }) => {
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
-    });
-
-    // Add resolve aliases to webpack config
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': './src',
-    };
-
-    // Optimize bundle size
-    if (!dev && !isServer) {
-      config.optimization = {
-        ...config.optimization,
-        mergeDuplicateChunks: true,
-        minimize: true,
-        splitChunks: {
-          chunks: 'all',
-          minSize: 20000,
-          minRemainingSize: 0,
-          minChunks: 1,
-          maxAsyncRequests: 30,
-          maxInitialRequests: 30,
-          enforceSizeThreshold: 50000,
-          cacheGroups: {
-            default: false,
-            vendors: false,
-            commons: {
-              name: 'commons',
-              chunks: 'all',
-              minChunks: 2,
-              reuseExistingChunk: true,
-            },
-            lib: {
-              test: /[\\/]node_modules[\\/]/,
-              name(module) {
-                const packageName = module.context.match(
-                  /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-                )[1];
-                return `npm.${packageName.replace('@', '')}`;
-              },
-              chunks: 'all',
-              minChunks: 1,
-              reuseExistingChunk: true,
-              priority: 1,
-            },
-          },
-        },
-      };
-    }
-
+  webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
         fs: false,
         module: false,
       };
     }
-
     return config;
   },
-  swcMinify: true,
 };
 
 export default nextConfig;
